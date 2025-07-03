b1e2a27191c6e2402c59c1528940dfb1003630c1
azhagu-swe <azhagu.swe@gmail.com>
2025-07-03 18:31:42 +0530

    Add Min Stack solution and update README for Day 95 with problem statement and implementation details

---
+++ Day-95/MinStack.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val); // Track minimum
+        }
+    }
+
+    public void pop() {
+        if (stack.pop().equals(minStack.peek())) {
+            minStack.pop(); // Pop min if matched
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println("Min: " + minStack.getMin()); // -3
+        minStack.pop();
+        System.out.println("Top: " + minStack.top());    // 0
+        System.out.println("Min: " + minStack.getMin()); // -2
+    }
+}
